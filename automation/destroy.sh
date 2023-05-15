790c4123c7a17c0410d12cd28f1c6d0b2196ce6a
Diego Vasque Maldonado <diegoomal13@gmail.com>
2023-05-15 14:37:52 -0300

    update

--- automation/destroy.sh
+++ automation/destroy.sh
@@ -3,3 +3,11 @@ echo "1) Remove conda env"
 conda deactivate
 
 conda remove --name snakegame-env --all -y
+
+
+
+echo "2) Exclude dir and files"
+
+rm -rf .pytest_cache
+rm -rf src/game/__pycache__
+rm -rf test/__pycache__
